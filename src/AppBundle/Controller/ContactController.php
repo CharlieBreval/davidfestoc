1095ea25cf7b3792b64bb87d3f58baf43add154e
Charlie Breval <cbreval@lafourchette.com>
2017-08-21 17:37:35 +0200

    OK

--- src/AppBundle/Controller/ContactController.php
+++ src/AppBundle/Controller/ContactController.php
@@ -10,7 +10,27 @@ class ContactController extends Controller
 {
     public function indexAction(Request $request)
     {
+        $success = false;
+        if ($request->getMethod() === 'POST') {
+
+            $body = '<html><body>'.nl2br($request->request->get('message'))."</body></html>";
+
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Contact from davidfestoc.com')
+                ->setFrom($request->request->get('email'))
+                ->setTo('charliebreval@yahoo.fr')
+                ->setBody($body,'text/html')
+            ;
+            $mailStatus = $this->get('mailer')->send($message);
+
+            if($mailStatus) {
+                $success = true;
+            }
+        }
+
+
         return $this->render('AppBundle:Contact:index.html.twig', [
+            'success' => $success
         ]);
     }
 }
